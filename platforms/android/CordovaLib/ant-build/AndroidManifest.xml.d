c126dbbd0188f2868724e668c64812a7716b1779
kpnatapo <kpnatapo@kmitl.ac.th>
2014-05-02 14:52:36 +0700

    test
    test

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\testGap\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\testGap\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\testGap\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
